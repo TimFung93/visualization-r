00e69763aa2c123ccb9a123641e503978a5a1d55
timfung93 <timothy.fung.04@gmail.com>
2017-09-08 15:45:31 -0400

    first push

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+const port = 8080;
+
+
+
+
+app.use(express.static('public'))
+
+
+
+
+
+app.listen(port, () => {
+	console.log('server started')
+})
