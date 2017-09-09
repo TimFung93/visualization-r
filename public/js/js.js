f39223785c90c4571229e71aa9a41f9e093bd1aa
timfung93 <timothy.fung.04@gmail.com>
2017-09-09 17:20:44 -0400

    added oil visualization

--- public/js/js.js
+++ public/js/js.js
@@ -1,6 +1,5 @@
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-    	console.log('this works')
         window.location.href = '/chorddiag'
     }
 }
--- server.js
+++ server.js
@@ -8,11 +8,18 @@ const port = process.env.PORT || 5000
 
 const publicPath = path.join(__dirname, '/public');
 
+app.use(express.static(publicPath))
+
+
+
 app.get('/chorddiag', function (req, res) {
   res.sendFile(publicPath + '/chorddiag.html');
 });
 
-app.use(express.static('public'))
+app.get('/oil', function (req, res) {
+  res.sendFile(publicPath + '/oil.html');
+});
+
 
 
 
